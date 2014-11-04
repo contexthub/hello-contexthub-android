1a5320650423c941fb874d0796f63db418127d02
Andrew Dyer <abdyer@gmail.com>
2014-11-04 15:01:25 -0600

    Add AppCompat library and Material theme/icons.

--- HelloContextHub/src/main/java/com/contexthub/hellocontexthub/MainActivity.java
+++ HelloContextHub/src/main/java/com/contexthub/hellocontexthub/MainActivity.java
@@ -1,15 +1,16 @@
 package com.contexthub.hellocontexthub;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Window;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         setProgressBarIndeterminate(true);
 
         if(savedInstanceState == null) {
